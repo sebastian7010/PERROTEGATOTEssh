cc0ad51b8ada351d808dcaeb914755ec0372fec8
“sebastian7010” <sebastianssc01@gmial.com>
2025-03-23 02:42:19 -0500

    productos json ids corregidos

--- categories.js
+++ categories.js
@@ -3,8 +3,8 @@ export const categories = [
     { id: 2, title: "Comida premium para perros" },
     { id: 3, title: "Comida para perros" },
     { id: 4, title: "Juguetes para Perros" },
-    { id: 5, title: "Alimentos para Gatos" },
-    { id: 6, title: "Juguetes para Gatos" },
-    { id: 7, title: "Accesorios para Aves" },
-    { id: 8, title: "Accesorios para Peces" }
+    { id: 5, title: "Accesorios para Gatos" },
+    { id: 6, title: "Comida premium para Gatos" },
+    { id: 7, title: "Comida para Gatos" },
+    { id: 8, title: "Juguetes para Gatos" }
 ];
